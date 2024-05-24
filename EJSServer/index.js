9e1e1a0a44ea492604c6d3bc3f12b1000ea94b3c
GagandeepLubana <gagandeeplubana1221@gmail.com>
2024-05-23 23:26:45 -0400

    ejs server practice

---
+++ EJSServer/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    
+
+    console.log(`server listening at ${port}`);
+});
+
+
+app.get("/", (req, res) => {
+    const d = new Date();
+    const day = d.getDay();
+    let type = "a weekday"
+    let adv = "it's time to work hard"
+
+    if (day === 0 || day === 6) {
+        type = "the weekend";
+        adv = "it's time to have some fun";
+    }
+    res.render("index.ejs", {dayType: type, advice: adv});
+});
+
